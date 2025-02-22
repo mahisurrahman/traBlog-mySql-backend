e283f717f6023d215b6f87cf70a49160ed06e9d7
mahisurrahman <zordanblog009@gmail.com>
2025-02-22 16:29:30 +0600

    shifting to home

--- services/userServices/userServices.js
+++ services/userServices/userServices.js
@@ -1,6 +1,30 @@
+const connectToDb = require("../../database/db.js");
+
 module.exports = {
+  
   async getAllUserService() {
     try {
+        const db = await connectToDb();
+
+        const q = "SELECT * FROM users";
+        const results = await new Promise((resolve, reject) => {
+          db.query(q, (err, data) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(data);
+            }
+          });
+        });
+
+        if(results.length > 0){
+          return{
+            status: 200,
+            error: false,
+            data: results,
+            message: "Get All Users Success",
+          }
+        }
         
     } catch (error) {
       console.log("Get All User Service Error", error);
