b21427de5fc8185b978126adde52c37420556f77
mahisurrahman <zordanblog009@gmail.com>
2025-02-23 15:27:36 +0600

    Register User Phase One Completed

--- services/authServices/authServices.js
+++ services/authServices/authServices.js
@@ -1,41 +1,88 @@
 const connectToDb = require("../../database/db.js");
+const bcrypt = require("bcryptjs");
+const queryAsync = require("../../utils/queryAsyncFunction/queryAsync.js");
 
 module.exports = {
   async registerUserService(body) {
-    console.log("Body Data", body);
     try {
       const db = await connectToDb();
-      console.log(db, "Connect To DB");
+
+      // Ensure Database Model Exists
+      const createTableQuery = `
+        CREATE TABLE IF NOT EXISTS users (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          firstName VARCHAR(225) NOT NULL,
+          lastName VARCHAR(225) NOT NULL,
+          userImg VARCHAR(225) NOT NULL,
+          gender INT NOT NULL,
+          userRole INT NOT NULL,
+          username VARCHAR(50) NOT NULL UNIQUE,
+          email VARCHAR(100) NOT NULL UNIQUE,
+          password TEXT NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          is_active BOOLEAN DEFAULT TRUE,
+          is_deleted BOOLEAN DEFAULT FALSE
+        );`;
+      await queryAsync(db, createTableQuery);
 
       // Check Existing User
-      const q = "SELECT * FROM users WHERE email = ? OR userName = ?";
-      const results = await new Promise((resolve, reject) => {
-        db.query(q, [body.email, body.userName], (err, data) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(data);
-          }
-        });
-      });
-
-      if (results.length > 0) {
+      const checkQuery = "SELECT * FROM users WHERE email = ? OR username = ?";
+      const existingUsers = await queryAsync(db, checkQuery, [
+        body.email,
+        body.username,
+      ]);
+      if (existingUsers.length > 0) {
+        db.end();
         return {
           status: 409,
           error: false,
           message: "User Already Exists",
-          data: results,
+          data: existingUsers,
         };
       }
 
-      // Close the database connection
-      db.end();
+      // Hashing Password
+      const salt = bcrypt.genSaltSync(10);
+      const hash = bcrypt.hashSync(body.password, salt);
+
+      // Insert user's data to the DB
+      const insertQuery = `
+        INSERT INTO users (firstName, lastName, userRole, userImg, gender, username, email, password)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+      const values = [
+        body.firstName,
+        body.lastName,
+        body.userRole,
+        body.userImg,
+        body.gender,
+        body.username,
+        body.email,
+        hash,
+      ];
+      const insertResult = await queryAsync(db, insertQuery, values);
+
+      // Make sure insertId exists
+      if (!insertResult.insertId) {
+        return{
+          status: 400,
+          error: true,
+          message: "Failed to insert user",
+          data: null,
+        }
+      }
+
+      // Fetch the new user data
+      const fetchUserQuery = "SELECT * FROM users WHERE id = ?";
+      const newUserResults = await queryAsync(db, fetchUserQuery, [
+        insertResult.insertId,
+      ]);
+      const newUser = newUserResults[0];
 
       return {
-        status: 200,
+        status: 201,
         error: false,
-        message: "User can be registered",
-        data: null,
+        message: "User created successfully",
+        data: newUser,
       };
     } catch (error) {
       console.log("Register User Service Error", error);
---
+++ utils/queryAsyncFunction/queryAsync.js
@@ -0,0 +1,11 @@
+function queryAsync(db, query, values = []) {
+    return new Promise((resolve, reject) => {
+      db.query(query, values, (err, results) => {
+        if (err) return reject(err);
+        resolve(results);
+      });
+    });
+  }
+
+  
+  module.exports = queryAsync;
